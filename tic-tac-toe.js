2e85166292ef1af48e37212fac4a6a4fe6a48f24
Sanaa Thompson <thompsonsanaa48@gmail.com>
2025-10-04 21:08:19 -0500

    Exercise 2:Add X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,27 @@
+//excercise 1
 document.addEventListener('DOMContentLoaded', () => {
+
 	const board = document.getElementById('board');
 	const squares = board.querySelectorAll('div');
-	squares.forEach(square => {
-		square.classList.add('square');
+	squares.forEach(sq => sq.classList.add('square'));
+	
+	//exercise 2
+	let currentPlayer= 'X';
+	const state= Array(9).fill('');
+
+	squares.forEach((square, index) => {
+		square.addEventListener('click', () => {
+			if (!state[index]) {
+				state[index]= currentPlayer;
+				square.textContent= currentPlayer;
+				square.classList.add(currentPlayer);
+
+				currentPlayer= currentPlayer == 'X' ? 'O' : 'X';
+			}
+		});
 	});
 });
+
+	
+
+
