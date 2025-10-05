50e25da1ba98abaac675e9c1022413ee574e86ff
Sanaa Thompson <thompsonsanaa48@gmail.com>
2025-10-04 21:45:07 -0500

    Exercise 4: Check for the winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,26 +8,48 @@ document.addEventListener('DOMContentLoaded', () => {
 	//exercise 2
 	let currentPlayer= 'X';
 	const state= Array(9).fill('');
+	let gameOver= false;
+
+	//exercise 4
+	const statusDiv= document.getElementById('status');
+	const wins= [
+		[0,1,2],[3,4,5],[6,7,8], //rows
+		[0,3,6],[1,4,7],[2,5,8], //columns
+		[0,4,8],[2,4,6] //diagonals
+	];
+
+	function checkWinner() {
+		for (const [a, b, c] of wins) {
+			if (state[a] && state[a] == state[b] && state[b] == state[c]) {
+				return state[a]; //get a 'x' or 'o'
+			}
+		}
+		return null;
+	}
 
 	squares.forEach((square, index) => {
 		square.addEventListener('click', () => {
+			if (gameOver) return;
 			if (!state[index]) {
 				state[index]= currentPlayer;
 				square.textContent= currentPlayer;
 				square.classList.add(currentPlayer);
+
+				const winner= checkWinner();
+				if (winner) {
+					statusDiv.textContent= `Congratulations! ${winner} is the Winner!`;
+					statusDiv.classList.add('you-won');
+					gameOver= true;
+					return;
+				}
 
 				currentPlayer= currentPlayer == 'X' ? 'O' : 'X';
 			}
 		});
 
 		//exercise 3
-		square.addEventListener('mouseover', () => {
-			square.classList.add('hover');
-		});
-
-		square.addEventListener('mouseout', () => {
-			square.classList.remove('hover');
-		});
+		square.addEventListener('mouseover', () => square.classList.add('hover'));
+		square.addEventListener('mouseout', () => square.classList.remove('hover'));
 	});
 });
 
