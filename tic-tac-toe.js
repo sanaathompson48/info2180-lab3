39e3e10a251613afc5c90e9dae8acb45cfc4c46b
Sanaa Thompson <thompsonsanaa48@gmail.com>
2025-10-04 23:27:58 -0500

    Exercise 6: Disallow Cheating

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,16 +1,13 @@
-//excercise 1
 document.addEventListener('DOMContentLoaded', () => {
 
 	const board = document.getElementById('board');
 	const squares = board.querySelectorAll('div');
 	squares.forEach(sq => sq.classList.add('square'));
 	
-	//exercise 2
 	let currentPlayer= 'X';
 	let state= Array(9).fill('');
 	let gameOver= false;
 
-	//exercise 4
 	const statusDiv= document.getElementById('status');
 	const wins= [
 		[0,1,2],[3,4,5],[6,7,8], //rows
@@ -20,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	function checkWinner() {
 		for (const [a, b, c] of wins) {
-			if (state[a] && state[a] == state[b] && state[b] == state[c]) {
+			if (state[a] && state[a] === state[b] && state[b] === state[c]) {
 				return state[a]; //get a 'X' or 'O'
 			}
 		}
@@ -43,16 +40,21 @@ document.addEventListener('DOMContentLoaded', () => {
 					return;
 				}
 
-				currentPlayer= currentPlayer == 'X' ? 'O' : 'X';
+				currentPlayer= currentPlayer === 'X' ? 'O' : 'X';
 			}
 		});
 
-		//exercise 3
-		square.addEventListener('mouseover', () => square.classList.add('hover'));
-		square.addEventListener('mouseout', () => square.classList.remove('hover'));
+		square.addEventListener('mouseover', () => {
+			if (!gameOver && !state[index]) {
+				square.classList.add('hover');
+			}
+		});
+		square.addEventListener('mouseout', () => {
+			square.classList.remove('hover');
+		});
 	});
 
-	//exercise 5
+
 	const newGameButton= document.querySelector('.btn');
 
 	newGameButton.addEventListener('click', () => {
